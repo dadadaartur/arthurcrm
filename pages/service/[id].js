4ddc656c1201f4eee3f1b43a907519a8f7fb593c
dadadaartur <dadadaartur@gmail.com>
2026-05-19 00:11:28 +0300

    Update [id].js

--- pages/service/[id].js
+++ pages/service/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
-import { supabase } from '../../lib/supabaseClient'
+import { supabase } from '../../supabaseClient'
 import Link from 'next/link'
 
 export default function ServiceDetail() {
