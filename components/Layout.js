f3c3389a6402b0cbb350a218c3dc28c32a634e57
dadadaartur <dadadaartur@gmail.com>
2026-05-20 16:53:19 +0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="bg-transparent relative z-10">
-        <div className="max-w-7xl mx-auto px-6 py-5 flex justify-start">
+        <div className="max-w-5xl mx-auto px-4 py-5 flex justify-start">
           <Link href="/" className="text-2xl font-bold tracking-tight select-none"
             style={{
               color: 'transparent',
