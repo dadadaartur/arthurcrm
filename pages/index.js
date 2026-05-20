ba4ff298fb7d05e65e078bd8e9cd3d29c54a97a1
dadadaartur <dadadaartur@gmail.com>
2026-05-20 08:25:58 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
         maxWidth: '540px',
         width: '100%',
         position: 'relative',
-        zIndex: 100,  // гарантированно поверх всех фонов
+        zIndex: 100,  // гарантированно над всеми фонами
         overflow: 'hidden',
         backgroundImage: `url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noise)' opacity='0.04'/%3E%3C/svg%3E")`
       }}>
