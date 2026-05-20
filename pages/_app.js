9ac9e7242d236f77ca790be5bdaac40a924ba0e6
dadadaartur <dadadaartur@gmail.com>
2026-05-20 15:56:07 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import Background from '../components/Background'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Background />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
