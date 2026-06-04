0c3331d85d2f3e81c58dcb5ccaa138e1255ff20e
dadadaartur <dadadaartur@gmail.com>
2026-06-04 19:36:24 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,35 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 import Background from '../components/Background'
+import { useEffect, useState } from 'react'
+import { supabase } from '../lib/supabaseClient'
 
 export default function App({ Component, pageProps }) {
+  const [profile, setProfile] = useState(null)
+  const [loadingProfile, setLoadingProfile] = useState(true)
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data: { user } }) => {
+      if (user) {
+        supabase
+          .from('profiles')
+          .select('display_name, role_id, company_id, avatar_url, first_name, last_name')
+          .eq('user_id', user.id)
+          .maybeSingle()
+          .then(({ data }) => {
+            setProfile(data)
+            setLoadingProfile(false)
+          })
+      } else {
+        setLoadingProfile(false)
+      }
+    })
+  }, [])
+
   return (
     <>
       <Background />
-      <Layout>
+      <Layout profile={profile} loadingProfile={loadingProfile}>
         <Component {...pageProps} />
       </Layout>
     </>
