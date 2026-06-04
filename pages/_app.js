427e5b650b07d96838904f4dd61435f008df148c
dadadaartur <dadadaartur@gmail.com>
2026-06-04 23:51:17 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,15 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 import Background from '../components/Background'
-import { createServerClient } from '../lib/supabaseClient'
-
-export async function getServerSideProps({ req }) {
-  const cookie = req.headers.cookie || ''
-  const supabaseServer = createServerClient(cookie)
-
-  const { data: { user } } = await supabaseServer.auth.getUser()
-
-  let profile = null
-  if (user) {
-    const { data } = await supabaseServer
-      .from('profiles')
-      .select('display_name, role_id, company_id, avatar_url, first_name, last_name')
-      .eq('user_id', user.id)
-      .maybeSingle()
-    profile = data
-  }
-
-  return {
-    props: {
-      user,
-      profile,
-    },
-  }
-}
+import { ProfileProvider } from '../context/ProfileContext'
 
 export default function App({ Component, pageProps }) {
-  const { user, profile, ...restPageProps } = pageProps
-
   return (
-    <>
+    <ProfileProvider>
       <Background />
-      <Layout user={user} profile={profile}>
-        <Component {...restPageProps} />
+      <Layout>
+        <Component {...pageProps} />
       </Layout>
-    </>
+    </ProfileProvider>
   )
 }
