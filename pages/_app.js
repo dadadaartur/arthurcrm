5ffc9c5868dfa193a78c23b5362060f917863dd5
dadadaartur <dadadaartur@gmail.com>
2026-06-04 20:01:29 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,40 +3,36 @@ import Layout from '../components/Layout'
 import Background from '../components/Background'
 import { createServerClient } from '../lib/supabaseClient'
 
-export default function App({ Component, pageProps, initialUser, initialProfile }) {
-  return (
-    <>
-      <Background />
-      <Layout user={initialUser} profile={initialProfile}>
-        <Component {...pageProps} />
-      </Layout>
-    </>
-  )
-}
-
-App.getInitialProps = async (appContext) => {
-  // getInitialProps для страницы, если есть
-  let pageProps = {}
-  if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx)
-  }
-
+export async function getServerSideProps(context) {
   const supabaseServer = createServerClient()
   const { data: { user } } = await supabaseServer.auth.getUser()
 
   let profile = null
   if (user) {
-    const { data } = await supabaseServer
+    profile = await supabaseServer
       .from('profiles')
       .select('display_name, role_id, company_id, avatar_url, first_name, last_name')
       .eq('user_id', user.id)
       .maybeSingle()
-    profile = data
   }
 
   return {
-    pageProps,
-    initialUser: user,
-    initialProfile: profile,
+    props: {
+      initialUser: user,
+      initialProfile: profile,
+    },
   }
+}
+
+export default function App({ Component, pageProps }) {
+  const { initialUser, initialProfile, ...restPageProps } = pageProps
+
+  return (
+    <>
+      <Background />
+      <Layout user={initialUser} profile={initialProfile}>
+        <Component {...restPageProps} />
+      </Layout>
+    </>
+  )
 }
