81f6f916e4fe65596a2ae15afd65acc2ff117576
dadadaartur <dadadaartur@gmail.com>
2026-06-09 21:06:14 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,17 @@ import Background from '../components/Background'
 import { ProfileProvider } from '../context/ProfileContext'
 
 export default function App({ Component, pageProps }) {
+  // Проверяем, есть ли у страницы флаг обхода макета
   const bypassLayout = Component.bypassLayout || false
 
   return (
     <ProfileProvider>
       <Background />
       {bypassLayout ? (
+        // Страница логина рендерится напрямую, без макета
         <Component {...pageProps} />
       ) : (
+        // Остальные страницы защищены макетом
         <Layout>
           <Component {...pageProps} />
         </Layout>
